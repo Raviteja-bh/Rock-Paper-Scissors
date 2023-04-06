9eb7eb30e2b67f5a2e8b62cff849dfed2666767e
Raviteja-bh <ravitejabh9790@gmail.com>
2023-04-06 15:29:05 +0530

    Add javascript code for the Rock-Paper-Scissors Project

---
+++ interactive.js
@@ -0,0 +1,40 @@
+let playRound = (computerChoice, playerChoice) => {
+    if(computerChoice == "rock" && playerChoice == "scissors"){
+        return "Compter wins bcoz Rock beats Scissors :(";
+    }else if(computerChoice == "paper" && playerChoice == "rock"){
+        return "Computer wins bcoz Paper beats Rock :(";
+    }else if(computerChoice == "scissors" && playerChoice == "paper"){
+        return "Computer wins bcoz Scissors beats Paper :(";
+    }else if(computerChoice == "scissors" && playerChoice == "rock"){
+        return "You win! bcoz Rock beats Scissors :)";
+    }else if(computerChoice == "rock" && playerChoice == "paper"){
+        return "You win! bcoz Paper beats Rock :)";
+    }else if(computerChoice == "paper" && playerChoice == "scissors"){
+        return "You win! bcoz Scissors beats Paper";
+    }else {
+        return "Whoops! Tie.";
+    }
+};
+
+function game(){
+    for(let i = 0; i<3 ; i++){
+        console.log(playRound(computer(), player()));
+    }
+}
+
+function getComputerChoice() {
+    const arr = ["Rock", "Paper", "Scissors"];
+    let num = parseInt(Math.random() * 3);
+    // console.log(arr[num]);
+    return arr[num].toLowerCase();
+}
+
+function getPlayerChoice() {
+    let ans = prompt("Enter your choice of word: Rock, Paper, Scissors: ");
+    return ans.toLowerCase();
+}
+
+const computer = getComputerChoice;
+const player = getPlayerChoice;
+
+game();
