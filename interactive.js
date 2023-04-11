f9a07911af0dfa44b023251051a92111df7f07e2
Raviteja-bh <ravitejabh9790@gmail.com>
2023-04-11 21:03:29 +0530

    Added JavaScript for interactive Webpage.

--- interactive.js
+++ interactive.js
@@ -5,30 +5,34 @@ var typed = new Typed(".welcome", {
     backSpeed: 150,
 });
 
-//Rock-Paper_Scissors Logic.
-let playRound = (computerChoice, playerChoice) => {
-    if(computerChoice == "rock" && playerChoice == "scissors"){
-        return "Compter wins bcoz Rock beats Scissors :(";
-    }else if(computerChoice == "paper" && playerChoice == "rock"){
-        return "Computer wins bcoz Paper beats Rock :(";
-    }else if(computerChoice == "scissors" && playerChoice == "paper"){
-        return "Computer wins bcoz Scissors beats Paper :(";
-    }else if(computerChoice == "scissors" && playerChoice == "rock"){
-        return "You win! bcoz Rock beats Scissors :)";
-    }else if(computerChoice == "rock" && playerChoice == "paper"){
-        return "You win! bcoz Paper beats Rock :)";
-    }else if(computerChoice == "paper" && playerChoice == "scissors"){
-        return "You win! bcoz Scissors beats Paper";
-    }else {
-        return "Whoops! Tie.";
-    }
-};
+let botcounter = 0, ycounter = 0;
 
-// function game(){
-//     for(let i = 0; i<3 ; i++){
-//         console.log(playRound(computer(), player()));
-//     }
-// }
+var btn = document.getElementById("start").addEventListener('click', ()=>{
+    var x = document.getElementById("rps1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }else{
+        x.style.display = "none";
+        const resty = document.getElementById('yscore');
+        const restbot = document.getElementById('bscore');
+        const remText = document.getElementById("endtext");
+        remText.innerHTML = "";
+        resty.innerText = 0;        
+        restbot.innerText = 0;
+        botcounter = 0;
+        ycounter = 0;
+        document.getElementById("rock").disabled = false;
+        document.getElementById("paper").disabled = false;
+        document.getElementById("scissors").disabled = false;
+
+    }
+})
+
+
+
+const yscore1 = document.getElementById('yscore');
+// console.log(yscore1.textContent);
+const botscore1 = document.getElementById('bscore');
 
 function getComputerChoice() {
     const arr = ["Rock", "Paper", "Scissors"];
@@ -37,12 +41,84 @@ function getComputerChoice() {
     return arr[num].toLowerCase();
 }
 
-// function getPlayerChoice() {
-//     let ans = prompt("Enter your choice of word: Rock, Paper, Scissors: ");
-//     return ans.toLowerCase();
-// }
+let rpss = document.querySelectorAll(".rps");
+for(let i=0; i<rpss.length; i++){
+    rpss[i].addEventListener('click', getPlayerChoice);
+}
 
-const computer = getComputerChoice;
-const player = getPlayerChoice;
+function getPlayerChoice(e){
+    const delText = document.getElementById("endtext");
+    delText.innerText="";
+    playRound(getComputerChoice(), e.target.id.toLowerCase());
+    getComputerChoice();
+    console.log(e.target.id.toLowerCase());
+    return e.target.id;
+      
+}
 
-// game();
+let playRound = (computer , player) => {
+    if(computer == "rock" && player == "scissors"){
+        botcounter++;
+        console.log(botcounter+" "+ycounter);
+        botscore1.innerText = botcounter.toString();
+        yscore1.innerText = ycounter.toString();
+    }else if(computer == "paper" &&  player =="rock"){
+        botcounter++;
+        console.log(botcounter+" "+ycounter);
+        botscore1.innerText = botcounter.toString();
+        yscore1.innerText = ycounter.toString();
+    }else if(computer == "scissors" && player == "paper"){
+        botcounter++;
+        console.log(botcounter+" "+ycounter);
+        botscore1.innerText = botcounter.toString();
+        yscore1.innerText = ycounter.toString();
+    }else if(computer == "scissors" && player == "rock"){
+        ycounter++;
+        console.log(botcounter+" "+ycounter);
+        yscore1.innerText = ycounter.toString();
+        botscore1.innerText = botcounter.toString();
+    }else if(computer == "rock" && player == "paper"){
+        ycounter++;
+        console.log(botcounter+" "+ycounter);
+        yscore1.innerText = ycounter.toString();
+        botscore1.innerText = botcounter.toString();
+    }else if(computer == "paper" && player == "scissors"){
+        ycounter++;
+        console.log(botcounter+" "+ycounter);
+        yscore1.innerText = ycounter.toString();
+        botscore1.innerText = botcounter.toString();
+    }else {
+        var tie = new Typed(".endTextIn", {
+            strings: ["Whoops! That's a tie."],
+            typeSpeed: 150,
+            backSpeed: 150,
+        })
+    }
+
+    if(botcounter == 5 || ycounter == 5){
+        if(botcounter == 5){
+            console.log("Aliens win");
+            document.getElementById("rock").disabled = true;
+            document.getElementById("paper").disabled = true;
+            document.getElementById("scissors").disabled = true;
+            var aliens = new Typed(".endTextIn", {
+                strings: ["Aliens Won! They will take over earth. :("],
+                typeSpeed: 150,
+                backSpeed: 150,
+            });
+        }else if(ycounter == 5){
+            console.log("Humans win");
+            document.getElementById("rock").disabled = true;
+            document.getElementById("paper").disabled = true;
+            document.getElementById("scissors").disabled = true;
+            var newTyped = new Typed(".endTextIn", {
+                strings: ["Humans Won! You made it, Saved earth."],
+                typeSpeed: 150,
+                backSpeed: 150,
+            });
+        }
+    } 
+}
+
+
+
